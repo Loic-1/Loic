1c864bc07ebd77bee1f2ef73bfc44abd7fa6d72d
Loic-1 <loic.vasile130@gmail.com>
2020-02-10 16:45:25 +0100

    j'ai rajoute une feuille de style

--- index.php
+++ index.php
@@ -2,20 +2,30 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>Table de multiplication</title>
+        <title>Stage de 3ème en entreprise</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-    <?php
-            $number = 2;
+        <header>
+            <img src="logo.jpg">
+            <h1>LOIC EST EN STAGE</h1>
+        </header>
+        <main>
+            <article>
 
-            for($i = 1; $i < 11;$i++){
-                echo
-                    "<p>",$number,
-                    "&times; ", $i,
-                    " = ", $number*$i,
-                    "</p>";
-            }
-    ?>
+                <h2>MON STAGE</h2>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolore sint iusto cupiditate architecto quam adipisci. Voluptate, eos quod non inventore, illum, est exercitationem aspernatur rem alias doloremque temporibus ad.</p>
+                <p>Quod illo sit exercitationem nesciunt, fuga consequatur magni nobis amet fugit ut laboriosam! Ex, veniam placeat hic perspiciatis minima consequatur quas, est nam dolore architecto, necessitatibus cumque quidem fuga voluptas?</p>
+                <p>Tempora beatae neque animi dolorum eveniet recusandae maxime aliquid numquam iure eligendi excepturi, nulla itaque, accusamus aliquam modi ipsam quos sequi similique. Alias, magnam ipsam praesentium qui libero in atque?</p>
+                
+                
+            </article>
+        </main>
+        <footer>
+
+
+        </footer>
     </body>
 </html>
 
